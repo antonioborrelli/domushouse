2040ad28212a9fb69717eabb733cbe5dae4104f6
benedettalz <benedettalazazzera@gmail.com>
2017-01-26 17:03:12 +0100

    file javascript per la gestione degli eventi nella vista impostazioni

--- view/script/settings.js
+++ view/script/settings.js
@@ -4,6 +4,52 @@
  * Componente utilizzato per gestire le impostazioni dell'utente
  * ======================================================================== */
 
+	//Elimina utente
+    function delete_utente(id){
+        var dataSet = [];
+        var arrayData=	'{'+
+                        '"operazione" : "delete_utente",' +
+                        '"id" : "'+id+'"}' ;
+
+        // INIZIO CHIAMATA AJAX
+        $("#myLoader").modal("show");
+        $.ajax({
+			  // definisco il tipo della chiamata
+			  type: "POST",
+			  // specifico la URL della risorsa da contattare
+			  url: "../controller/query_utente.php",
+			  // passo dei dati alla risorsa remota
+			  data: JSON.parse(arrayData),
+			  // definisco il formato della risposta
+			  dataType: "html",
+			  // imposto un'azione per il caso di successo
+			  success: function(risposta){
+				  
+				console.log(risposta);
+
+                if(risposta == 1){
+                    alert('Eliminazione avvenuto con successo!');
+                }
+
+				location.reload();
+
+                //NASCONDO IL LOADER
+				$("#myLoader").modal("hide");
+
+			  },// ed una per il caso di fallimento
+			  error: function(){
+			    alert("Si è verificato un errore riprovare più tardi!!!");
+                //NASCONDO IL LOADER
+				$("#myLoader").modal("hide");
+			  }
+		});
+        // FINE CHIAMATA AJAX
+
+    }
+
+
+
+
 $(function() {
 
 
@@ -29,7 +75,13 @@ $(function() {
 						  var data= result.data
 
 						  for (i=0; i<data.length; i++){
-							  var riga= '<tr><th scope="row">'+data[i].id+'</th><td>'+data[i].username+'</td><td>'+data[i].password+'</td></tr>';
+							  var riga="";
+
+							  if(data[i].username == 'admin')
+							  	riga= '<tr><th scope="row">'+data[i].id+'</th><td>'+data[i].username+'</td><td>'+data[i].password+'</td></td></tr>';
+							  else
+							  	riga= '<tr><th scope="row">'+data[i].id+'</th><td>'+data[i].username+'</td><td>'+data[i].password+'</td><td style="text-align: center;"><button type="button" onclick="delete_utente('+data[i].id+');" class="btn btn-danger btn-xs"><span class="glyphicon glyphicon-trash"></span></button></td></tr>';
+							  
 							  $("#contenuto_tabella").append(riga);
 
 						  }
@@ -82,12 +134,8 @@ $(function() {
 			  dataType: "html",
 			  // imposto un'azione per il caso di successo
 			  success: function(risposta){
-				  
-				console.log(risposta);
 
-                if(risposta == 1){
-                    alert('Inserimento avvenuto con successo!');
-                }
+				location.reload();
 
                 //NASCONDO IL LOADER
 				$("#myLoader").modal("hide");
@@ -126,8 +174,9 @@ $(function() {
 			  success: function(risposta){
 				  
 				console.log(risposta);
-
-                if(risposta == 1){
+				var result = JSON.parse(risposta);
+				console.log(result.stato);
+                if(result != null && result.stato == 1){
                     alert('Aggiornamento avvenuto con successo!');
                 }
 
